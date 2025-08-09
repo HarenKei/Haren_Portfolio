dc034031d1c3df9f6dfaad04911eaef074e46623
HarenKei <seungmin1266@gmail.com>
2025-08-09 17:26:21 +0900

    chore : change name of directory and component

---
+++ src/components/sections/main/MainSection.tsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+import "./MainSection.css";
+
+const NAMES: string[] = [
+  "정승민",
+  "Jeong Seungmin",
+  "鄭丞民",
+  "ジョン スンミン",
+  "Haren",
+];
+
+const MainSection: React.FC = () => {
+  const [currentNameIndex, setCurrentNameIndex] = useState(0);
+  const [isAnimating, setIsAnimating] = useState(false);
+
+  useEffect(() => {
+    const slideInterval = setInterval(() => {
+      setIsAnimating(true);
+
+      setTimeout(() => {
+        setCurrentNameIndex((prevIndex) => (prevIndex + 1) % NAMES.length);
+        setIsAnimating(false);
+      }, 300);
+    }, 3000);
+
+    return () => clearInterval(slideInterval);
+  }, []);
+
+  return (
+    <div className="about-section">
+      <h1>안녕하세요</h1>
+      <h1>신입 프론트엔드 개발자</h1>
+      <h1 className="name-container">
+        <div className={`name-slide ${isAnimating ? "slide-out" : "slide-in"}`}>
+          <span className="name-text">{NAMES[currentNameIndex]}</span>
+        </div>
+      </h1>
+      <h1>입니다.</h1>
+    </div>
+  );
+};
+
+export default MainSection;
